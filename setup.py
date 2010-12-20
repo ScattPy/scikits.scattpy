5817d4b62930a01e87ed9890fe890d2f34a24472
Alexander Vinokurov <alexander.a.vinokurov@gmail.com>
2010-12-20 19:42:42 +0300

    fixed setup.py: removed kmatrix_f

--- setup.py
+++ setup.py
@@ -56,9 +56,6 @@ def configuration(parent_package='', top_path=None, package_name=DISTNAME):
     config.add_extension('f_utils',
                          sources=[os.path.join('src', 'f_utils.for')]
                          )
-    config.add_extension('kmatrix_f',
-                         sources=[os.path.join('src', 'kmatrix_f.for')]
-                         )
 
     return config
 
