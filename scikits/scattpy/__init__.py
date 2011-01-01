8c1d8813a7c35a74a6a5e28e60b5b45765a02edc
Alexander Vinokurov <alexander.a.vinokurov@gmail.com>
2011-01-02 00:06:48 +0300

    fixed import * issue

--- scikits/scattpy/__init__.py
+++ scikits/scattpy/__init__.py
@@ -1,6 +1,22 @@
 """ScattPy is a SciKits package providing numerical methods of solving
 light scattering by non-spherical particles problem."""
 from methods import svm
-from laboratory import *
-
-__all__ = ['laboratory','methods']
+from laboratory import \
+		Shape,\
+		ShapeSphere,\
+		ShapeSpheroid,\
+		ShapeChebyshev,\
+		Particle,\
+		HomogeneousParticle,\
+		Sphere,\
+		ProlateSpheroid,\
+		OblateSpheroid,\
+		ChebParticle,\
+		LayeredParticle,\
+		Layered_EqShape_Particle,\
+		Layered_EqShape_EqVolume_Particle,\
+		EffMedium_Particle,\
+		EffMedium_MaxwellGarnett_Particle,\
+		EffMedium_InvMaxwellGarnett_Particle,\
+		EffMedium_Bruggeman_Particle,\
+		Lab
