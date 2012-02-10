c6bd001f1f0fced184a1967eac8832cee3694f9a
maxal <maxim.kolmakov@gmail.com>
2012-02-10 16:50:07 +0400

    New test for oblate spheroid. Reformat code.

--- test/svm_test.py
+++ test/svm_test.py
@@ -4,19 +4,30 @@ from spheroidal import *
 from spheroidal_particles import *
 from properties import *
 
-
+#main integration tests for non-absorbing particle
 class testSVM(unittest.TestCase):
-    def test_svm(self):
+    def test_svm1(self):
         nmax = 5
         c1 = 1.0
         c2 = 1.0
-        alpha = pi/4
+        alpha = pi / 4
         particle = ProlateSpheroid()
-        b_sca = getSolution(particle,TMInputWave(pi/4),c1, c2, nmax)[0]
-
+        b_sca = getSolution(particle, TMInputWave(alpha), c1, c2, nmax)[0]
         k = 1
         C_ext = getCext(particle, alpha, 1, b_sca, nmax)[0]
-        C_sca = getCsca(k,b_sca,nmax)[0]
+        C_sca = getCsca(k, b_sca, nmax)[0]
+        self.assertAlmostEqual(C_ext, C_sca, 7)
+
+    def test_svm2(self):
+        nmax = 3
+        c1 = 2.0
+        c2 = 2.0
+        alpha = pi / 2
+        particle = OblateSpheroid()
+        b_sca = getSolution(particle, TMInputWave(alpha), c1, c2, nmax)[0]
+        k = 4
+        C_ext = getCext(particle, alpha, 1, b_sca, nmax)[0]
+        C_sca = getCsca(k, b_sca, nmax)[0]
         self.assertAlmostEqual(C_ext, C_sca, 7)
 
 if __name__ == '__main__':
